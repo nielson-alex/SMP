7da6abd7276db15652c0844bad145e6d802deaac
Alex Nielson <neils58@byui.edu>
2018-12-21 17:31:00 -0700

    Added sort feature, search feature, and cleaned up the Index, Create, and Edit pages

--- SMP/Pages/Meetings/Index.cshtml.cs
+++ SMP/Pages/Meetings/Index.cshtml.cs
@@ -18,11 +18,45 @@ namespace SMP.Pages.Meetings
             _context = context;
         }
 
+        public string SubjectSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
         public IList<Meeting> Meeting { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            Meeting = await _context.Meeting.ToListAsync();
+            SubjectSort = String.IsNullOrEmpty(sortOrder) ? "Subject1_desc" : "";
+            DateSort = sortOrder == "MeetingDate" ? "MeetingDate_desc" : "MeetingDate";
+            CurrentFilter = searchString;
+
+            IQueryable<Meeting> ID = from m in _context.Meeting
+                                            select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ID = ID.Where(m => m.Subject1.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "Subject1_desc":
+                    ID = ID.OrderByDescending(m => m.Subject1);
+                    break;
+                case "MeetingDate":
+                    ID = ID.OrderBy(m => m.MeetingDate);
+                    break;
+                case "MeetingDate_desc":
+                    ID = ID.OrderByDescending(m => m.MeetingDate);
+                    break;
+                default:
+                    ID = ID.OrderBy(s => s.MeetingDate);
+                    break;
+            }
+
+            Meeting = await ID.AsNoTracking().ToListAsync();
+            //Meeting = await _context.Meeting.ToListAsync();
         }
     }
 }
